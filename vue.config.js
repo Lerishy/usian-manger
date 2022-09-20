d814b99fa461254624a8d0cf9b701a3c810e15da
Lerishy <2230008711@qq.com>
2022-09-20 21:07:07 +0800

    feat(配置标题、ico图标、serve): 配置标题等等

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 项目打包之后白屏问题
+  publicPath : "./",
+  devServer: {
+    // 端口号
+    port: 8888,
+    // 主机名
+    host: "localhost",
+    // 启动项目时是否自动打开浏览器
+    open: true,
+    // 是否开启https
+    https: false,
+    // 配置跨域
+   
+  },
+  // 关闭eslint
+  lintOnSave : false
 });
