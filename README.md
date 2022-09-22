095becfe78e72066a280d6274fe352874d0961be
Lerishy <2230008711@qq.com>
2022-09-22 20:42:40 +0800

    feat: 增加了登录页面布局和渲染退出功能

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-# usian-manger
+# usian-manager-system
 
 ## Project setup
 ```
-yarn install
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-yarn serve
+npm run serve
 ```
 
 ### Compiles and minifies for production
 ```
-yarn build
+npm run build
 ```
 
 ### Lints and fixes files
 ```
-yarn lint
+npm run lint
 ```
 
 ### Customize configuration
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+// 引入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "./style/reset.css"
+import "./style/common.css"
+
+// 页面权限
+import "./permission"
+
+// 注册element-ui
 Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
+
 
 new Vue({
   router,
